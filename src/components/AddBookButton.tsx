06088cf1223afdc226eb863a6ed3daa4c9de174d
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-22 20:47:45 +0500

    working on te handle input change function

--- src/components/AddBookButton.tsx
+++ src/components/AddBookButton.tsx
@@ -20,3 +20,30 @@ const AddBookButton: React.FC<AddBookButtonProps> = ({ onAddBook }) => {
     images: ['https://images.pexels.com/photos/1765033/pexels-photo-1765033.jpeg'],
   });
   
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({ ...prev, [name]: value }));
+  };
+  
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onAddBook({
+      ...formData,
+      id: Date.now().toString(),
+      createdAt: new Date(),
+      status: 'Available',
+    });
+    setIsModalOpen(false);
+    // Reset form
+    setFormData({
+      title: '',
+      author: '',
+      isbn: '',
+      condition: '',
+      genre: '',
+      description: '',
+      city: '',
+      exactLocation: '',
+      images: ['https://images.pexels.com/photos/1765033/pexels-photo-1765033.jpeg'],
+    });
+  };
